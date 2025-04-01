b8d07061840ae13fa737af23073e22598c09ae30
Trung <tnguyen571@my.bcit.ca>
2025-04-01 11:46:22 -0700

    commit

--- dashboard/stats.js
+++ dashboard/stats.js
@@ -1,10 +1,10 @@
 /* UPDATE THESE VALUES TO MATCH YOUR SETUP */
 
-const PROCESSING_STATS_API_URL = "http://99.79.60.105:8100/stats"
+const PROCESSING_STATS_API_URL = "http://3.99.189.212:8100/stats"
 const ANALYZER_API_URL = {
-    stats: "http://99.79.60.105:8110/stats",
-    energy_consumption: "http://99.79.60.105:8110/events/energy-consumption",
-    solar_generation: "http://99.79.60.105:8110/events/solar-generation"
+    stats: "http://3.99.189.212:8110/stats",
+    energy_consumption: "http://3.99.189.212:8110/events/energy-consumption",
+    solar_generation: "http://3.99.189.212:8110/events/solar-generation"
 }
 
 // Function to generate a random integer for the index parameter  
